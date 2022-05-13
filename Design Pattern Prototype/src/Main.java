6fc804b2aba806dd604d8d5db79aa896aa21720d
DiegoSoftfobia <diego.cappai@softfobia.com>
2022-05-13 15:53:56 +0200

    other change design prototype

--- Design Pattern Prototype/src/Main.java
+++ Design Pattern Prototype/src/Main.java
@@ -1,12 +1,20 @@
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 
-		ShapeCache.loadCache();
+		StudentDao prove = new StudentDao();
+		List<Student> prove2 = prove.clone();
+		prove2.add(new Student("S3", "Clone", "Clone"));
 		
+		for (Student element : prove.getAllStudent()) {
+			System.out.println(element);
+		}
 		
-		
+		for (Student element : prove2) {
+			System.out.println(element);
+		}
 	}
 
 }
